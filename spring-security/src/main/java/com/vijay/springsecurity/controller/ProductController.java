4a1689cd63036aafb68d609a20950242e819b372
Vijay-Daraji <darajivijay542@gmail.com>
2023-02-23 13:08:05 +0530

    authorized method level

--- spring-security/src/main/java/com/vijay/springsecurity/controller/ProductController.java
+++ spring-security/src/main/java/com/vijay/springsecurity/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.vijay.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +17,13 @@ public class ProductController {
 		return "welcome to endpoint is not secure";
 	}
 	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@GetMapping("/product/all")
 	public String getProduct() {
 		return "All Product";
 	}
 	
-	@GetMapping("/")
-	public String getMessage() {
-		return "Welcome";
-	}
-	
+	@PreAuthorize("hasAuthority('ROLE_USER')")
 	@GetMapping("/product/{id}")
 	public String getMessageById(@PathVariable int id) {
 		return "Welcome user "+id;
