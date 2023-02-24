046a72639fb8fa6bcd8ceb518652b3fb318dad48
Vijay-Daraji <darajivijay542@gmail.com>
2023-02-24 09:53:47 +0530

    remove extra code

--- spring-security/src/main/java/com/vijay/springsecurity/entity/UserInfo.java
+++ spring-security/src/main/java/com/vijay/springsecurity/entity/UserInfo.java
@@ -4,11 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 public class UserInfo {
